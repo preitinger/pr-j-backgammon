74c2ff80c817769f649251a66d606d2dd3abb4a3
preitinger <peter.reitinger@gmail.com>
2025-10-28 09:43:04 +0100

    update source paths in buld.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd $(dirname $0)
 
-paths="pr pr/backgammon pr/backgammon/ui pr/backgammon/control pr/backgammon/spin pr/backgammon/spin/ui pr/backgammon/gnubg"
+paths="pr pr/backgammon pr/backgammon/control pr/backgammon/gnubg pr/backgammon/gnubg/model pr/backgammon/jokers pr/backgammon/jokers/control pr/backgammon/jokers/model pr/backgammon/jokers/view pr/backgammon/model pr/backgammon/spin pr/backgammon/spin/control pr/backgammon/spin/control/workers pr/backgammon/spin/model pr/backgammon/spin/templatesearchers pr/backgammon/spin/trackmove pr/backgammon/spin/view pr/backgammon/view pr/control pr/cutscreenshot pr/cv pr/http pr/model pr/res pr/res/board pr/res/gnubg pr/view"
 
 args=""
 for item in $paths
